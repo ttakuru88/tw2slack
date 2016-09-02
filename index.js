df3ec8ad762745efc6b2b132c72db62e9f5d2c8d
Takuru <takuru@hai3.net>
2016-09-02 11:10:41 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+let exec = require('child_process').exec;
+
+exports.tw2slack = (event, context, callback) => {
+    if (!event.cmd) {
+        return callback('Please specify a command to run as event.cmd');
+    }
+    const child = exec(event.cmd, (error) => {
+        // Resolve with result of process
+        callback(error, 'Process complete!');
+    });
+
+    // Log process stdout and stderr
+    child.stdout.on('data', console.log);
+    child.stderr.on('data', console.error);
+};
