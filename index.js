44b53ffdcf3ba9f61b794901e729196060608734
Takuru <takuru@hai3.net>
2016-09-09 11:02:05 +0900

    ES6

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-'use strict';
+'use strict'
+
 var aws = require('aws-sdk')
-var Twitter = require('twitter');
-var Slack = require('slack-node');
+var Twitter = require('twitter')
+var Slack = require('slack-node')
 
 var slack = new Slack();
-slack.setWebhook('https://hooks.slack.com/services/');
+slack.setWebhook('https://hooks.slack.com/services/')
 
 var twitterClient = new Twitter({
   consumer_key: '',
   consumer_secret: '',
   access_token_key: '',
   access_token_secret: ''
-});
+})
 
 aws.config.region = 'ap-northeast-1';
 var s3 = new aws.S3({ apiVersion: '2006-03-01' });
@@ -20,52 +21,62 @@ exports.tw2slack = (event, context, callback) => {
   s3.getObject({
     Bucket: 'tw2slack',
     Key: 'max_tweet_id'
-  }, function(err, data) {
+  }, (err, data) => {
     if(err) {
-      console.error(err);
-      context.done();
+      console.error(err)
+      context.done()
     }
     else {
-      var maxTweetId = data.Body.toString();
-      var nextMaxTweetId = maxTweetId;
+      var maxTweetId = data.Body.toString()
+      var nextMaxTweetId = maxTweetId
+      var newTweets = []
 
-      console.log("maxTweetId: " + maxTweetId);
+      console.log(`maxTweetId: ${maxTweetId}`)
 
-      twitterClient.get('search/tweets', {q: 'docbase lang:ja exclude:retweets'}, function(error, tweets, response) {
+      twitterClient.get('search/tweets', {q: 'docbase lang:ja exclude:retweets'}, (error, tweets, response) => {
         for(let i=tweets.statuses.length-1; i>=0; i--){
-          let tweet = tweets.statuses[i];
+          let tweet = tweets.statuses[i]
+
           if(tweet.id_str <= maxTweetId) {
-            continue;
+            continue
           }
           if(nextMaxTweetId < tweet.id_str) {
-            nextMaxTweetId = tweet.id_str;
+            nextMaxTweetId = tweet.id_str
           }
 
-          console.log("tweet2slack: " + tweet.id_str);
-
-          slack.webhook({
-            username: "@" + tweet.user.screen_name,
-            text: "```\n" + tweet.text + "\n```\nhttps://twitter.com/" + tweet.user.screen_name + '/status/' + tweet.id_str,
-            icon_emoji: tweet.user.profile_image_url_https
-          }, function(err, response) {
-            console.log(response);
-          });
+          newTweets.push(tweet)
         }
 
-        console.log("nextMaxTweetId: " + nextMaxTweetId);
+        console.log(`nextMaxTweetId: ${nextMaxTweetId}`)
 
-        s3.putObject({
-          Bucket: 'tw2slack',
-          Key: 'max_tweet_id',
-          ContentType: 'text/plain',
-          Body: nextMaxTweetId
-        }, function(err, data){
-          if (err) {
-            console.error(err);
-          }
-          context.done();
-        });
-      });
+        var tweetedCount = 0;
+        for(let i=0; i<newTweets.length; i++){
+          let tweet = newTweets[i]
+          let tweetUrl = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+          console.log(`tweet2slack: ${tweetUrl}`)
+
+          slack.webhook({
+            username: `@${tweet.user.screen_name}`,
+            text: `\`\`\`\n${tweet.text}\n\`\`\`\n${tweetUrl}`,
+            icon_emoji: tweet.user.profile_image_url_https
+          }, (err, response) => {
+            tweetedCount++
+            if(tweetedCount >= newTweets.length){
+              s3.putObject({
+                Bucket: 'tw2slack',
+                Key: 'max_tweet_id',
+                ContentType: 'text/plain',
+                Body: nextMaxTweetId
+              }, (err, data) => {
+                if (err) {
+                  console.error(err)
+                }
+                context.done()
+              })
+            }
+          })
+        }
+      })
     }
-  });
-};
+  })
+}
